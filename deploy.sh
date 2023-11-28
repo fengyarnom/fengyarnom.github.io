d09003ac8c128475fbf65ed7c5a2af50ae4c6f05
FengYarnom <fengyarnom@gmail.com>
2023-11-28 16:16:02 +0800

    fix: modify deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,17 +13,3 @@ git push -f origin "master:gh-pages"
 # 清理临时目录
 cd ..
 rm -rf "$TEMP_DIR"
-
-
-TEMP_DIR=$(mktemp -d)
-cp -r sources/content/posts/* "$TEMP_DIR"
-cd "$TEMP_DIR"
-git init
-
-git add .
-git commit -m "Update: $current_datetime"
-git remote add origin "git@github.com:fengyarnom/fengyarnom.github.io.git"
-git push -f origin "master:posts"
-# 清理临时目录
-cd ..
-rm -rf "$TEMP_DIR"
